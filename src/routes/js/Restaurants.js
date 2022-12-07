4ba31116a271fd2419ee94820c31e5ed90aba3e7
tnpfldyd <dkdldhs0322@gmail.com>
2022-12-07 14:21:05 +0900

    feat : font, ResDetail

--- src/routes/js/Restaurants.js
+++ src/routes/js/Restaurants.js
@@ -9,44 +9,46 @@ function Restaurants() {
   var baseURL = process.env.REACT_APP_BASE_URL
   const [restaurants, setRestaurants] = useState()
   const getRes = async () => {
-    const res = await axios.get(`${baseURL}/restaurant/`, {headers: { 'Content-Type': 'application/json' }})
+    const res = await axios.get(`${baseURL}/restaurant/`, { headers: { 'Content-Type': 'application/json' } })
     setRestaurants(res.data)
   }
-  
+
   // 랜더링시 레스토랑 정보 받아오기
   useEffect(() => {
     getRes()
   }, [])
 
   if (restaurants != null) {
-    restaurants.map((data)=>{
-      {data.images.map((img, i)=> {
-        console.log(decodeURIComponent(data.images[i].image.replace('http://127.0.0.1:8000/media/','')))
-      })}
-    }) 
+    restaurants.map((data) => {
+      {
+        data.images.map((img, i) => {
+          console.log(decodeURIComponent(data.images[i].image.replace('http://127.0.0.1:8000/media/', '')))
+        })
+      }
+    })
   }
-  
+
   return (
     <Container>
       <h6>현재위치</h6>
-      <Location/>
+      <Location />
       <form action="">
         <input type="text" />
-        <input type="submit" value="검색"/>
+        <input type="submit" value="검색" />
       </form>
       <Link to='/map'>지도로보기</Link>
       <div>
         <div>필터</div>
       </div>
       {
-        restaurants ? restaurants.map((data, idx)=>{
+        restaurants ? restaurants.map((data, idx) => {
           const res_idx = '/res_detail/' + data.id
-          return(
+          return (
             <div className='res-one' key={idx}>
               <div className='res-img-wrapper'>
-              {data.images.map((img, i)=> {
-                return(
-                  <img src={decodeURIComponent(data.images[i].image.replace('http://127.0.0.1:8000/media/',''))} className='res-img'/>
+                {data.images.map((img, i) => {
+                  return (
+                    <img src={decodeURIComponent(data.images[i].image.replace('http://127.0.0.1:8000/media/', ''))} className='res-img' />
                   )
                 })}
               </div>
--- src/routes/js/RestaurantsDetail.js
+++ src/routes/js/RestaurantsDetail.js
@@ -2,24 +2,27 @@ import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import { useEffect, useState } from 'react';
 import { Container, Form } from 'react-bootstrap';
-
+import '../css/ResDetail.css';
+import styled from '../../components/css/Button.module.css';
 function RestaurantsDetail() {
-  let {id} = useParams();
+  let { id } = useParams();
   var baseURL = process.env.REACT_APP_BASE_URL
   const [restaurant, setRestaurant] = useState()
   const [reviews, setReviews] = useState()
+  const [menus, setMenus] = useState()
 
   const getRes = async () => {
-    const res = await axios.get(`${baseURL}/restaurant/`, {headers: { 'Content-Type': 'application/json' }})
-    setRestaurant(res.data[id-1])
+    const res = await axios.get(`${baseURL}/restaurant/${id}/`, { headers: { 'Content-Type': 'application/json' } })
+    setRestaurant(res.data)
+    setMenus(res.data.detail.split(', '))
   }
-
   const getReviews = async () => {
     const review = await axios({
       method: "get",
       url: `${baseURL}/articles/${id}/review/`
     })
     setReviews(review.data)
+
   }
 
   // 랜더링시 레스토랑 정보 받아오기
@@ -39,24 +42,43 @@ function RestaurantsDetail() {
       }
     })
   }
-
-  console.log(reviews)
+  const detailDate = (a) => {
+    const milliSeconds = new Date() - a;
+    const seconds = milliSeconds / 1000;
+    if (seconds < 60) return `방금 전`;
+    const minutes = seconds / 60;
+    if (minutes < 60) return `${Math.floor(minutes)}분 전`;
+    const hours = minutes / 60;
+    if (hours < 24) return `${Math.floor(hours)}시간 전`;
+    const days = hours / 24;
+    if (days < 7) return `${Math.floor(days)}일 전`;
+    const weeks = days / 7;
+    if (weeks < 5) return `${Math.floor(weeks)}주 전`;
+    const months = days / 30;
+    if (months < 12) return `${Math.floor(months)}개월 전`;
+    const years = days / 365;
+    return `${Math.floor(years)}년 전`;
+  };
   return (
     <Container>
-      <div>디테일</div>
-      <div>{id}</div>
       {
-        restaurant ? 
+        restaurant ?
           <>
-            <div>식당번호 : {restaurant.id} | 식당명 : {restaurant.name} | 카테고리 : {restaurant.category_name} </div>
-            <div className='res-img-wrapper'>
-              {restaurant.images.map((img, i)=> {
+            <div className='res-img-wrapper res-img-div'>
+              {restaurant.images.map((img, i) => {
                 return (
-                    <img src={decodeURIComponent(restaurant.images[i].image.replace('http://127.0.0.1:8000/media/',''))} className='res-img'/>
-                  )
-                })}
-            </div>  
-            <div>메뉴 : {restaurant.detail}</div>
+                  <img src={decodeURIComponent(restaurant.images[i].image.replace('http://127.0.0.1:8000/media/', ''))} className='res-img' />
+                )
+              })}
+            </div>
+            <button className={styled.category}>{restaurant.category_name}</button>
+            <h3 className={styled.name}>
+              {restaurant.name}
+            </h3>
+            <p>인기메뉴</p>
+            {menus.map((menu) =>
+              <p className={styled.menuname}>{menu}</p>
+            )}
           </>
           : null
       }
@@ -64,10 +86,10 @@ function RestaurantsDetail() {
         reviews ?
           <>
             <h3>식당리뷰</h3>
-            {reviews.map((el, i)=>{
-              return(
+            {reviews.map((el, i) => {
+              return (
                 <div>
-                  <div>{reviews[i].user} | {reviews[i].title} | {reviews[i].content} | {reviews[i].grade} | {reviews[i].updated_at} | </div>
+                  <div>{reviews[i].user} | {reviews[i].title} | {reviews[i].content} | {reviews[i].grade} | {detailDate(new Date(reviews[i].updated_at))} | </div>
                 </div>
               )
             })}
@@ -84,7 +106,7 @@ function RestaurantsDetail() {
         />
         <Form.Control
           onChange={1}
-          type="email"
+          type="text"
           placeholder='식당리뷰'
           className='mb-3'
         />
